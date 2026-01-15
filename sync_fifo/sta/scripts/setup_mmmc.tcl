a6fda7a770879c5eeb3f70f37b95940506b0c026
lucasval-vasic <lucas.valentin-garcia@analog.com>
2026-01-16 00:34:21 +0100

    fix lib paths, remove cdB libs, use CCS-Timing instead

--- sync_fifo/sta/scripts/setup_mmmc.tcl
+++ sync_fifo/sta/scripts/setup_mmmc.tcl
@@ -4,48 +4,42 @@
 
 # slow timing UPF
 create_library_set -name ls_wc_upf -timing { \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/D_CELLS_JIHD/v4_1/liberty_LPMOS/v4_1_1/PVT_1_80V_range/D_CELLS_JIHD_LPMOS_slow_1_62V_125C.lib \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/IO_CELLS_F3V/v2_1/liberty_UPF_LPMOS/v2_1_0/PVT_1_80V_3_30V_range/IO_CELLS_F3V_LPMOS_UPF_slow_1_62V_3_00V_125C.lib \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/spram/XSPRAMLP_128X16_M8P/v4_0_2/liberty_UPF_LPMOS/XSPRAMLP_128X16_M8P_UPF_slow_1_62V_125C.lib } \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/D_CELLS_JIHD/v4_1/liberty_LPMOS/v4_1_1/PVT_1_80V_range/D_CELLS_JIHD_LPMOS_slow_1_62V_125C.lib \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/IO_CELLS_F3V/v2_1/liberty_UPF_LPMOS/v2_1_0/PVT_1_80V_3_30V_range/IO_CELLS_F3V_LPMOS_UPF_slow_1_62V_3_00V_125C.lib \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/spram/XSPRAMLP_128X16_M8P/v4_0_2/liberty_UPF_LPMOS/XSPRAMLP_128X16_M8P_UPF_slow_1_62V_125C.lib } \
     -si { \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/IO_CELLS_F3V/IO_CELLS_F3V_LPMOS_slow_1_62V_3_00V_125C.cdB \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/D_CELLS_JIHD/D_CELLS_JIHD_LPMOS_slow_1_62V_125C.cdB }
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/IO_CELLS_F3V/IO_CELLS_F3V_LPMOS_slow_1_62V_3_00V_125C.cdB \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/D_CELLS_JIHD/D_CELLS_JIHD_LPMOS_slow_1_62V_125C.cdB }
 
 # fast timing UPF
 create_library_set -name ls_bc_upf -timing { \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/D_CELLS_JIHD/v4_1/liberty_LPMOS/v4_1_1/PVT_1_80V_range/D_CELLS_JIHD_LPMOS_fast_1_98V_m40C.lib \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/IO_CELLS_F3V/v2_1/liberty_UPF_LPMOS/v2_1_0/PVT_1_80V_3_30V_range/IO_CELLS_F3V_LPMOS_UPF_fast_1_98V_3_60V_m40C.lib \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/spram/XSPRAMLP_128X16_M8P/v4_0_2/liberty_UPF_LPMOS/XSPRAMLP_128X16_M8P_UPF_fast_1_98V_m40C.lib } \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/D_CELLS_JIHD/v4_1/liberty_LPMOS/v4_1_1/PVT_1_80V_range/D_CELLS_JIHD_LPMOS_fast_1_98V_m40C.lib \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/IO_CELLS_F3V/v2_1/liberty_UPF_LPMOS/v2_1_0/PVT_1_80V_3_30V_range/IO_CELLS_F3V_LPMOS_UPF_fast_1_98V_3_60V_m40C.lib \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/spram/XSPRAMLP_128X16_M8P/v4_0_2/liberty_UPF_LPMOS/XSPRAMLP_128X16_M8P_UPF_fast_1_98V_m40C.lib } \
     -si { \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/IO_CELLS_F3V/IO_CELLS_F3V_LPMOS_fast_1_98V_3_60V_m40C.cdB \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/D_CELLS_JIHD/D_CELLS_JIHD_LPMOS_fast_1_98V_m40C.cdB }
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/IO_CELLS_F3V/IO_CELLS_F3V_LPMOS_fast_1_98V_3_60V_m40C.cdB \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/D_CELLS_JIHD/D_CELLS_JIHD_LPMOS_fast_1_98V_m40C.cdB }
 
 # slow timing CCST
 create_library_set -name ls_wc_ccst -timing { \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/D_CELLS_JIHD/v4_1/liberty_CCS_TIMING_LPMOS/v4_1_1/PVT_1_80V_range/D_CELLS_JIHD_LPMOS_ccs_timing_slow_1_62V_125C.lib.gz \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/IO_CELLS_F3V/v2_1/liberty_CCS_TIMING_LPMOS/v2_1_0/PVT_1_80V_3_30V_range/IO_CELLS_F3V_LPMOS_ccs_timing_slow_1_62V_3_00V_125C.lib.gz \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/spram/XSPRAMLP_128X16_M8P/v4_0_2/liberty_UPF_LPMOS/XSPRAMLP_128X16_M8P_UPF_slow_1_62V_125C.lib } \
-    -si { \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/IO_CELLS_F3V/IO_CELLS_F3V_LPMOS_slow_1_62V_3_00V_125C.cdB \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/D_CELLS_JIHD/D_CELLS_JIHD_LPMOS_slow_1_62V_125C.cdB }
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/D_CELLS_JIHD/v4_1/liberty_CCS_TIMING_LPMOS/v4_1_1/PVT_1_80V_range/D_CELLS_JIHD_LPMOS_ccs_timing_slow_1_62V_125C.lib.gz \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/IO_CELLS_F3V/v2_1/liberty_CCS_TIMING_LPMOS/v2_1_0/PVT_1_80V_3_30V_range/IO_CELLS_F3V_LPMOS_ccs_timing_slow_1_62V_3_00V_125C.lib.gz \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/spram/XSPRAMLP_128X16_M8P/v4_0_2/liberty_UPF_LPMOS/XSPRAMLP_128X16_M8P_UPF_slow_1_62V_125C.lib } \
 
 # fast timing CCST
 create_library_set -name ls_bc_ccst -timing { \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/D_CELLS_JIHD/v4_1/liberty_CCS_TIMING_LPMOS/v4_1_1/PVT_1_80V_range/D_CELLS_JIHD_LPMOS_ccs_timing_fast_1_98V_m40C.lib.gz \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/IO_CELLS_F3V/v2_1/liberty_CCS_TIMING_LPMOS/v2_1_0/PVT_1_80V_3_30V_range/IO_CELLS_F3V_LPMOS_ccs_timing_fast_1_98V_3_60V_m40C.lib.gz \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/spram/XSPRAMLP_128X16_M8P/v4_0_2/liberty_UPF_LPMOS/XSPRAMLP_128X16_M8P_UPF_fast_1_98V_m40C.lib } \
-    -si { \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/IO_CELLS_F3V/IO_CELLS_F3V_LPMOS_fast_1_98V_3_60V_m40C.cdB \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/D_CELLS_JIHD/D_CELLS_JIHD_LPMOS_fast_1_98V_m40C.cdB }
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/D_CELLS_JIHD/v4_1/liberty_CCS_TIMING_LPMOS/v4_1_1/PVT_1_80V_range/D_CELLS_JIHD_LPMOS_ccs_timing_fast_1_98V_m40C.lib.gz \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/IO_CELLS_F3V/v2_1/liberty_CCS_TIMING_LPMOS/v2_1_0/PVT_1_80V_3_30V_range/IO_CELLS_F3V_LPMOS_ccs_timing_fast_1_98V_3_60V_m40C.lib.gz \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/spram/XSPRAMLP_128X16_M8P/v4_0_2/liberty_UPF_LPMOS/XSPRAMLP_128X16_M8P_UPF_fast_1_98V_m40C.lib } \
 
 # slow timing CCSP
 create_library_set -name ls_wc_ccsp -timing { \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/D_CELLS_JIHD/v4_1/liberty_CCS_POWER_LPMOS/v4_1_1/PVT_1_80V_range/D_CELLS_JIHD_LPMOS_ccs_power_slow_1_62V_125C.lib.gz \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/IO_CELLS_F3V/v2_1/liberty_CCS_POWER_LPMOS/v2_1_0/PVT_1_80V_3_30V_range/IO_CELLS_F3V_LPMOS_ccs_power_slow_1_62V_3_00V_125C.lib.gz \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/spram/XSPRAMLP_128X16_M8P/v4_0_2/liberty_UPF_LPMOS/XSPRAMLP_128X16_M8P_UPF_slow_1_62V_125C.lib } \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/D_CELLS_JIHD/v4_1/liberty_CCS_POWER_LPMOS/v4_1_1/PVT_1_80V_range/D_CELLS_JIHD_LPMOS_ccs_power_slow_1_62V_125C.lib.gz \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/diglibs/IO_CELLS_F3V/v2_1/liberty_CCS_POWER_LPMOS/v2_1_0/PVT_1_80V_3_30V_range/IO_CELLS_F3V_LPMOS_ccs_power_slow_1_62V_3_00V_125C.lib.gz \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/spram/XSPRAMLP_128X16_M8P/v4_0_2/liberty_UPF_LPMOS/XSPRAMLP_128X16_M8P_UPF_slow_1_62V_125C.lib } \
     -si { \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/IO_CELLS_F3V/IO_CELLS_F3V_LPMOS_slow_1_62V_3_00V_125C.cdB \
-    /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/D_CELLS_JIHD/D_CELLS_JIHD_LPMOS_slow_1_62V_125C.cdB }
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/IO_CELLS_F3V/IO_CELLS_F3V_LPMOS_slow_1_62V_3_00V_125C.cdB \
+    /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/celticDB/D_CELLS_JIHD/D_CELLS_JIHD_LPMOS_slow_1_62V_125C.cdB }
 
 #-------------------------------------------------------------------------------
 #   create timing condition
@@ -65,19 +59,17 @@ create_library_set -name ls_wc_ccsp -timing { \
 
 create_rc_corner \
     -T 125 \
-    -cap_table /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/cadence/v8_0/capTbl/v8_0_1/xh018_xx51_MET5_METMID_max.capTbl \
     -name rc_cworst \
-    -qx_tech_file  /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/cadence/v8_0/QRC_pvs/v8_0_1_1/XH018_1151/QRC-Max/qrcTechFile
+    -qx_tech_file  /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/cadence/v8_0/QRC_pvs/v8_0_1_1/XH018_1151/QRC-Max/qrcTechFile
 
-create_delay_corner -name dc_wc_upf -rc_corner rc_cworst -library_set ls_wc_upf
+create_delay_corner -name dc_wc_upf -rc_corner rc_cworst -library_set ls_wc_ccst
 
 create_rc_corner \
     -T -40 \
-    -cap_table /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/cadence/v8_0/capTbl/v8_0_1/xh018_xx51_MET5_METMID_min.capTbl \
     -name rc_cbest \
-    -qx_tech_file /cadence_pdk/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/cadence/v8_0/QRC_pvs/v8_0_1_1/XH018_1151/QRC-Min/qrcTechFile
+    -qx_tech_file /eda/xfab/XKIT/x_all/cadence/XFAB_Digital_Power_RefKit-cadence/v1_3_1/pdk/xh018/cadence/v8_0/QRC_pvs/v8_0_1_1/XH018_1151/QRC-Min/qrcTechFile
 
-create_delay_corner -name dc_bc_upf -rc_corner rc_cbest -library_set ls_bc_upf
+create_delay_corner -name dc_bc_upf -rc_corner rc_cbest -library_set ls_bc_ccst
 
 #-------------------------------------------------------------------------------
 #   create constraint mode
